eda62750825602b07ae84b5c4d0f6a3ba52596f0
romaresh <shakunroman@gmail.com>
2017-02-28 19:38:40 +0300

    modife logIn form and about

---
+++ cinema-sync/app/controllers/users/sessions_controller.rb
@@ -0,0 +1,5 @@
+class Users::SessionsController < Devise::SessionsController
+  def create
+     super
+  end
+end
--- cinema-sync/config/routes.rb
+++ cinema-sync/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {:registrations => "users/registrations"}
+  devise_for :users, :controllers => {:registrations => "users/registrations", :sessions => "users/sessions"}
 
   root 'home#index'
 
