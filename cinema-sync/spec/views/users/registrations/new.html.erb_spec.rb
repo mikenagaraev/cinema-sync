713e2a49d45f361ece9a4d30b846fd5ba0e14178
nik-tar <wdoom96@gmail.com>
2017-04-11 14:37:52 +0300

    Added 2 more tests for reg/log in form

--- cinema-sync/spec/views/users/registrations/new.html.erb_spec.rb
+++ cinema-sync/spec/views/users/registrations/new.html.erb_spec.rb
@@ -13,4 +13,21 @@ describe 'Sing up page' do
 
   end
 
+  it "should create a new user and log in as it" do
+
+    visit "/"
+
+    click_link "Sign up"
+    expect(current_path).to eq "/users/sign_up"
+    fill_in('user_username', :with => 'CoolGuy')
+    fill_in('user_email', :with => 'tester@tester.by')
+    fill_in('user_password', :with => 'test-pass')
+    fill_in('user_password_confirmation', :with => 'test-pass')
+    click_button "Get Started"
+
+    expect(current_path).to eq "/"
+    expect(page).to have_content("Sign out")
+    
+  end
+
 end
--- cinema-sync/spec/views/users/sessions/new.html.erb_spec.rb
+++ cinema-sync/spec/views/users/sessions/new.html.erb_spec.rb
@@ -13,4 +13,31 @@ describe 'Log in page' do
 
   end
 
+  it "should log in as a user" do
+
+    visit "/"
+
+    click_link "Sign up"
+    expect(current_path).to eq "/users/sign_up"
+    fill_in('user_username', :with => 'CoolGuy')
+    fill_in('user_email', :with => 'tester@tester.by')
+    fill_in('user_password', :with => 'test-pass')
+    fill_in('user_password_confirmation', :with => 'test-pass')
+    click_button "Get Started"
+
+    expect(current_path).to eq "/"
+    expect(page).to have_content("Sign out")
+    click_link "Sign out"
+    expect(current_path).to eq "/"
+    click_link "Sign in"
+    expect(current_path).to eq "/users/sign_in"
+    fill_in('user_email', :with => 'tester@tester.by')
+    fill_in('user_password', :with => 'test-pass')
+    click_button "Log In"
+
+    expect(current_path).to eq "/"
+    expect(page).to have_content("Sign out")
+
+  end
+
 end
