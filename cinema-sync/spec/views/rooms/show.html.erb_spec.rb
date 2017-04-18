f21bd01ef04c97d9fe43f4b2acd1878b162f0977
nik-tar <wdoom96@gmail.com>
2017-04-18 14:03:22 +0300

    Fixed bugs in views and one test added for chat

--- cinema-sync/spec/views/rooms/show.html.erb_spec.rb
+++ cinema-sync/spec/views/rooms/show.html.erb_spec.rb
@@ -70,4 +70,38 @@ describe 'Chat' do
 
   end
 
+  it 'should not send messages from unregistered users' do
+
+    visit "/"
+
+    click_link "Sign up"
+    expect(current_path).to eq "/users/sign_up"
+    fill_in('user_username', :with => 'CoolGuy')
+    fill_in('user_email', :with => 'tester@tester.by')
+    fill_in('user_password', :with => 'test-pass')
+    fill_in('user_password_confirmation', :with => 'test-pass')
+    click_button "Get Started"
+
+    expect(current_path).to eq "/"
+    find('button.button-create').click
+
+    within('#create-room-pop-up') do
+      expect(page).to have_content('New Room')
+      fill_in('room_title', :with => 'My room')
+      click_button "Create"
+    end
+
+    expect(page).to have_content('My room')
+    room_path = current_path
+    click_link "Sign out"
+    expect(current_path).to eq "/"
+    expect(page).not_to have_content('Sign out')
+    expect(page).to have_content('Sign in')
+    click_link "My room"
+    expect(current_path).to eq room_path
+
+    expect(page).not_to have_css('span.input-group-btn')
+
+  end
+
 end
