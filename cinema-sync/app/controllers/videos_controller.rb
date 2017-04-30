ff5e5cb2cb2da98f1dfa9454c0c859bd28a3900c
MikhailLU <clowfinger13@gmail.com>
2017-04-30 19:53:39 +0300

    edit title room

--- cinema-sync/app/controllers/videos_controller.rb
+++ cinema-sync/app/controllers/videos_controller.rb
@@ -10,17 +10,18 @@ class VideosController < ApplicationController
   end
 
   def create
-    @video = Video.new(videos_params.merge(room_id: @room.id))
-    @video.save(videos_params)
-    redirect_to user_room_path(@user, @room)
+    @video = Video.create(videos_params.merge(room_id: @room.id))
+    redirect_to user_room_path(@user)
   end
 
   def update
     @video.update_attributes(videos_params)
+    redirect_to user_room_path(@user)
   end
 
   def destroy
     @video.destroy
+    redirect_to user_room_path(@user)
   end
 
   private
